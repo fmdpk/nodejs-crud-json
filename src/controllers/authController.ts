d19eabd7b4caeafbdfbf5ccf54bd4117eac4827b
farzin mossaed <fmossaed@gmail.com>
2025-03-11 12:30:11 +0330

    add signin with otp

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -29,6 +29,53 @@ export const signIn = async (req: Request, res: Response) => {
   }
 };
 
+export const signInWithOTP = async (req: Request, res: Response) => {
+  const { email } = req.body;
+
+  // Send OTP to the user's email
+  const { data, error } = await supabase.auth.signInWithOtp({
+    email,
+    options: {},
+  });
+
+  if (error) {
+    res.status(400).json({ error: error.message });
+  } else {
+    res.status(200).json({ message: "OTP sent to your email" });
+    console.log(data);
+  }
+};
+
+export const verifySignInOTP = async (req: Request, res: Response) => {
+  const { email, otp } = req.body;
+
+  // Verify OTP
+  const { data, error } = await supabase.auth.verifyOtp({
+    email,
+    token: otp, // Change 'otp' to 'token' if this is the correct parameter
+    type: "magiclink", // or 'magiclink' or 'recovery' based on your use case
+  });
+
+  if (error) {
+    res.status(400).json({ error: error.message });
+    return;
+  }
+
+  res.status(200).json({ message: "Sign-in successful", user: data });
+};
+
+export const enableMFA = async (req: Request, res: Response): Promise<void> => {
+  const { data, error } = await supabase.auth.mfa.enroll({
+    factorType: "totp",
+  });
+
+  if (error) {
+    res.status(400).json({ error: error.message });
+  } else {
+    res.status(200).json(data);
+  }
+};
+
 export const getUser = async (req: Request, res: Response) => {
   const { data, error } = await supabase.auth.getUser();
 
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,12 +1,23 @@
 import { Router } from "express";
-import { signUp, signIn, getUser, logout } from "../controllers/authController";
+import {
+  signUp,
+  signIn,
+  getUser,
+  logout,
+  enableMFA,
+  signInWithOTP,
+  verifySignInOTP,
+} from "../controllers/authController";
 import { authenticateUser } from "../middlewares/authMiddleware";
 
 const router = Router();
 
 router.post("/signup", signUp);
 router.post("/signin", signIn);
+router.post("/signin-otp", signInWithOTP);
+router.post("/verify-signin-otp", verifySignInOTP);
 router.get("/user", authenticateUser, getUser);
 router.post("/logout", authenticateUser, logout);
+router.post("/enable-mfa", authenticateUser, enableMFA);
 
 export default router;
