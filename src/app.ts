3e639c28c69a93ff2f2cb1bf0dcd730fff24daf0
farzin mossaed <fmossaed@gmail.com>
2025-03-18 09:15:50 +0330

    add supabase table creation command in supabase-query.txt

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(
-    `Swagger documentation available at http://localhost:${PORT}/api-docs`
+    `Swagger documentation available at http://localhost:${PORT}/swagger`
   );
 });
